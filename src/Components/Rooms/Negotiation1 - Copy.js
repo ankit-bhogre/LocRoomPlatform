070dc163ed697e1253f77bb57795c4d17bc963d0
Ankit Bhogre <tns.ankit@gmail.com>
2021-01-07 18:40:16 +0530

    changes

---
+++ src/Components/Rooms/Negotiation1 - Copy.js
@@ -0,0 +1,121 @@
+import React, { Component, Fragment } from 'react'; 
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import CSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
+import Tabs, { TabPane } from "rc-tabs";
+import TabContent from "rc-tabs/lib/SwipeableTabContent";
+import ScrollableInkTabBar from "rc-tabs/lib/ScrollableInkTabBar";
+import cx from "classnames";
+
+import ResizeDetector from "react-resize-detector";
+//import AppMain from "../Layout/AppMain";
+import AppHeader from "../../Layout/AppHeader/";
+import AppSidebar from "../../Layout/AppSidebar/";
+import PageTitle from "../../Layout/AppMain/PageTitle";
+import Footer from "../Footer"; 
+
+import timeline_img from "../../assets/custom_images/nego_timeline.png";
+import details_img from "../../assets/custom_images/nego_details.png";
+import proposal_img from "../../assets/custom_images/nego_proposal.png";
+import meeting_img from "../../assets/custom_images/nego_meeting.png";
+import attachment_img from "../../assets/custom_images/nego_attachment.png";
+import general_img from "../../assets/custom_images/nego_general.png";
+import { useTranslation } from 'react-i18next';
+
+//import { Route, Switch, Redirect } from 'react-router-dom';  
+class Negotiation1 extends Component {  
+   constructor(props) {
+    super(props);
+    this.state = {
+      closedSmallerSidebar: false,
+    };
+  }
+   
+ 
+  render() { 
+	 let {
+      colorScheme,
+      enableFixedHeader,
+      enableFixedSidebar,
+      enableFixedFooter,
+      enableClosedSidebar,
+      closedSmallerSidebar,
+      enableMobileMenu,
+      enablePageTabsAlt,
+    } = this.props;
+	const { t } = this.props;
+	
+	
+         return (
+			<Fragment>
+			<ResizeDetector
+				handleWidth
+				render={({ width }) => (
+					<div
+              className={cx(
+                "app-container app-theme-" + colorScheme,
+                { "fixed-header": enableFixedHeader },
+                { "fixed-sidebar": enableFixedSidebar || width < 1250 },
+                { "fixed-footer": enableFixedFooter },
+                { "closed-sidebar": enableClosedSidebar || width < 1250 },
+                {
+                  "closed-sidebar-mobile": closedSmallerSidebar || width < 1250,
+                },
+                { "sidebar-mobile-open": enableMobileMenu },
+                { "body-tabs-shadow-btn": enablePageTabsAlt }
+              )}>
+					  
+					  <AppHeader /> 
+						<div className="app-main">
+							<AppSidebar />
+							<div className="app-main__outer">
+								<div className="app-main__inner">
+									 <CSSTransitionGroup component="div" transitionName="TabsAnimation" transitionAppear={true} transitionAppearTimeout={0} transitionEnter={false} transitionLeave={false}>
+									<PageTitle heading="Negotiation Room" subheading="Negotiation 1" icon="pe-7s-door-lock"/>
+									<Tabs defaultActiveKey="1" renderTabBar={() => <ScrollableInkTabBar />} renderTabContent={() => <TabContent />}>
+										<TabPane tab={<span><img className="tab_head_img" src={timeline_img} alt="" /> Timeline</span>} key="1">
+											  Timeline  
+										</TabPane>
+										<TabPane tab={<span><img className="tab_head_img" src={details_img} alt="" /> Details</span>} key="2">
+											Details
+										</TabPane>
+										<TabPane tab={<span><img className="tab_head_img" src={proposal_img} alt="" /> Proposal</span>} key="3">
+											Proposal
+										</TabPane>
+										<TabPane tab={<span><img className="tab_head_img" src={meeting_img} alt="" /> Meetings</span>} key="4">
+											Meetings
+										</TabPane>
+										<TabPane tab={<span><img className="tab_head_img" src={attachment_img} alt="" /> Attachments</span>} key="5">
+											Attachments
+										</TabPane>
+										<TabPane tab={<span><img className="tab_head_img" src={general_img} alt="" /> General</span>} key="6">
+											General
+										</TabPane>
+									</Tabs>
+									</CSSTransitionGroup>
+								</div>
+							</div>
+						</div>
+				
+					</div>
+				)}
+			  />
+			<Footer />	
+            </Fragment>  
+        )  
+    }  
+}  
+
+
+  
+const mapStateToProp = (state) => ({
+  colorScheme: state.ThemeOptions.colorScheme,
+  enableFixedHeader: state.ThemeOptions.enableFixedHeader,
+  enableMobileMenu: state.ThemeOptions.enableMobileMenu,
+  enableFixedFooter: state.ThemeOptions.enableFixedFooter,
+  enableFixedSidebar: state.ThemeOptions.enableFixedSidebar,
+  enableClosedSidebar: state.ThemeOptions.enableClosedSidebar,
+  enablePageTabsAlt: state.ThemeOptions.enablePageTabsAlt,
+});
+
+export default withRouter(connect(mapStateToProp)(Negotiation1));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from 'react-dom';
 import "./assets/base.scss";
 import {
@@ -7,19 +7,25 @@ import {
   Route
 } from "react-router-dom";
 import Login from './Components/Login'; 
-import Home from './Components/Home'; 
+import Home from './Components/Home';  
+import News from './Components/News'; 
 import Negotiation1 from './Components/Rooms/Negotiation1'; 
-import configureStore from "./config/configureStore"; 
+import configureStore from "./config/configureStore";
+
+import './i18n';
 
 import { Provider } from "react-redux";
 const store = configureStore();
+
 export default function App() {
+	
   return (
     <Provider store={store}>
 		<Router>
 			<Switch>
 				<Route path="/negotiation1"><Negotiation1 /></Route>
 				<Route path="/home"><Home /></Route>
+				<Route path="/news"><Suspense fallback={(<div>Loading</div>)}><News /></Suspense></Route>
 				<Route path="/"><Login /></Route>
 			</Switch>
 		</Router>
