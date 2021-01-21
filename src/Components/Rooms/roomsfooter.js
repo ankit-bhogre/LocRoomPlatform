1e9c556247f8c719a453918ab751f6e93a8ac83d
suchit0011 <suchit0011@gmail.com>
2021-01-21 13:23:26 +0530

    new

--- src/Components/Rooms/roomsfooter.js
+++ src/Components/Rooms/roomsfooter.js
@@ -1,5 +1,6 @@
 import React  from 'react';
 import { Link } from "react-router-dom";
+import DropdownGridMenus  from './bottomdropdown';
 // import './index.scss';
 //import { Accordion,Card,Tab ,Dropdown} from 'react-bootstrap';
 //import Tabs, { TabPane } from "rc-tabs";
@@ -26,30 +27,12 @@ class Roomsfooter extends React.Component {
 render() {
     return (
         <div className="roomfooter-div">
+         
           <div className="roomft-content">
               <span className="roomft-titlename">Room belonging to </span> 
               <span>
-                <UncontrolledDropdown className="d-inline-block" direction="up" > 
-                    <DropdownToggle caret className="btn-primary roomft-dropbtn" color="">Info</DropdownToggle>
-                       <DropdownMenu className="dropdown-menu-lg title-dropdown">
-                              <div className="dropdown-menu-header">
-                                 <div className="dropdown-menu-header-inner">
-                                     <div className="menu-header-image" />
-                                         <div className="menu-header-content">
-                                         <h5 className="menu-header-title">Example</h5>
-                                    </div>
-                                </div>
-                             </div>
-                             <div className="grid-menu grid-menu-2col">
-                                 <Link class="dropdown-item roomft_innerheading" to="#">ACTIVITY</Link>
-                                 <Link class="dropdown-item" to="#"><i className="pe-7s-chat roomft_bottomicons"></i><span className="roomft_bottomtext">Chat</span></Link>
-                                 <Link class="dropdown-item" to="#"><i className="pe-7s-coffee roomft_bottomicons"></i><span className="roomft_bottomtext">Messages</span></Link>
-                                 <Link class="dropdown-item" to="#"><i className="pe-7s-video roomft_bottomicons"></i><span className="roomft_bottomtext">Videocall</span></Link>
-                                 <Link class="dropdown-item" to="#"><i className="pe-7s-key roomft_bottomicons"></i><span className="roomft_bottomtext">Owner</span></Link>
-                                 <Link class="dropdown-item" to="#"><i className="pe-7s-door-lock roomft_bottomicons"></i><span className="roomft_bottomtext">Room Info</span></Link>
-                            </div> 
-                        </DropdownMenu>
-                </UncontrolledDropdown>
+              <DropdownGridMenus />
+               
             </span>
           </div>
       </div>
